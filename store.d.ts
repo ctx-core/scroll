fb3c0ec52824dd848c557c607190074c818d9de9
Brian Takita <brian.takita@gmail.com>
2020-01-04 17:21:42 -0500

    feat: store: b__ factories

--- store.d.ts
+++ store.d.ts
@@ -1,5 +1,10 @@
-export declare const __active__Sticky__Scroll: import("svelte/store").Writable<{}>;
-export declare function add__active__Sticky__Scroll(key: any): void;
-export declare function remove__active__Sticky__Scroll(key: any): void;
-export declare function _active__active__Sticky__Scroll(key: any): any;
-export declare function _match__active__Sticky__Scroll(key: any, active: any): boolean;
+import { Writable } from 'svelte/store';
+export interface Writable__active__Sticky__Scroll extends Writable<any> {
+    add__active__Sticky__Scroll: (key: string) => void;
+    remove__active__Sticky__Scroll: (key: string) => void;
+    _active__active__Sticky__Scroll: (key: string) => void;
+    _match__active__Sticky__Scroll: (key: string, active: any) => void;
+}
+export declare const b__active__Sticky__Scroll: (ctx?: any, opts?: any) => Writable__active__Sticky__Scroll;
+export declare const __active__Sticky__Scroll: Writable__active__Sticky__Scroll;
+export declare const add__active__Sticky__Scroll: (key: string) => void, remove__active__Sticky__Scroll: (key: string) => void, _active__active__Sticky__Scroll: (key: string) => void, _match__active__Sticky__Scroll: (key: string, active: any) => void;
--- store.ts
+++ store.ts
@@ -1,35 +1,56 @@
-import { writable, get } from 'svelte/store'
-import { clone } from '@ctx-core/object'
+import { writable, get, Writable } from 'svelte/store'
+import { assign, clone, _b } from '@ctx-core/object'
 import { log } from '@ctx-core/logger'
 const logPrefix = '@ctx-core/scroll/store.js'
-export const __active__Sticky__Scroll = writable({})
-export function add__active__Sticky__Scroll(key) {
-	log(`${logPrefix}|add__active__Sticky__Scroll`)
-	__active__Sticky__Scroll.update(
-		__ => {
-			const active__Sticky__Scroll = clone(__)
-			active__Sticky__Scroll[key] = true
-			return active__Sticky__Scroll
-		})
+export interface Writable__active__Sticky__Scroll extends Writable<any> {
+	add__active__Sticky__Scroll:(key:string)=>void
+	remove__active__Sticky__Scroll:(key:string)=>void
+	_active__active__Sticky__Scroll:(key:string)=>void
+	_match__active__Sticky__Scroll:(key:string, active:any)=>void
 }
-export function remove__active__Sticky__Scroll(key) {
-	log(`${logPrefix}|remove__active__Sticky__Scroll`)
-	__active__Sticky__Scroll.update(
-		__ => {
-			const active__Sticky__Scroll = clone(__)
-			active__Sticky__Scroll[key] = false
-			return active__Sticky__Scroll
-		})
-}
-export function _active__active__Sticky__Scroll(key) {
-	log(`${logPrefix}|_active__active__Sticky__Scroll`)
-	const active__Sticky__Scroll = get(__active__Sticky__Scroll)
-	const active =
-		active__Sticky__Scroll
-		? active__Sticky__Scroll[key]
-		: false
-	return active
-}
-export function _match__active__Sticky__Scroll(key, active) {
-	return !!(active) == !!(_active__active__Sticky__Scroll(key))
-}
+export const b__active__Sticky__Scroll = _b<Writable__active__Sticky__Scroll>('__active__Sticky__Scroll', ()=>{
+	const __active__Sticky__Scroll = writable({})
+	return assign(__active__Sticky__Scroll, {
+		add__active__Sticky__Scroll,
+		remove__active__Sticky__Scroll,
+		_active__active__Sticky__Scroll,
+		_match__active__Sticky__Scroll,
+	})
+	function add__active__Sticky__Scroll(key) {
+		log(`${logPrefix}|add__active__Sticky__Scroll`)
+		__active__Sticky__Scroll.update(
+			__=>{
+				const active__Sticky__Scroll = clone(__)
+				active__Sticky__Scroll[key] = true
+				return active__Sticky__Scroll
+			})
+	}
+	function remove__active__Sticky__Scroll(key) {
+		log(`${logPrefix}|remove__active__Sticky__Scroll`)
+		__active__Sticky__Scroll.update(
+			__=>{
+				const active__Sticky__Scroll = clone(__)
+				active__Sticky__Scroll[key] = false
+				return active__Sticky__Scroll
+			})
+	}
+	function _active__active__Sticky__Scroll(key) {
+		log(`${logPrefix}|_active__active__Sticky__Scroll`)
+		const active__Sticky__Scroll = get(__active__Sticky__Scroll)
+		const active =
+			active__Sticky__Scroll
+			? active__Sticky__Scroll[key]
+			: false
+		return active
+	}
+	function _match__active__Sticky__Scroll(key, active) {
+		return !!(active) == !!(_active__active__Sticky__Scroll(key))
+	}
+})
+export const __active__Sticky__Scroll = b__active__Sticky__Scroll()
+export const {
+	add__active__Sticky__Scroll,
+	remove__active__Sticky__Scroll,
+	_active__active__Sticky__Scroll,
+	_match__active__Sticky__Scroll,
+} = __active__Sticky__Scroll
